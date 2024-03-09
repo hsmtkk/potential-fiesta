4c254c6d2e74365538f4d85e652cdcd6930b8975
hsmtkk <hsmtkk@gmail.com>
2024-03-09 19:15:51 +0900

    return env vars for test

--- front/app/api/test/route.ts
+++ front/app/api/test/route.ts
@@ -3,5 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest): Promise<NextResponse> {
     console.log("output env vars")
     console.log(process.env)
-    return NextResponse.json({ message: "ok" })
+    return NextResponse.json({ env_vars: JSON.stringify(process.env) })
 }
