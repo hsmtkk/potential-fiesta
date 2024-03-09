d367e209a3893002c73e3e0266472cae52d9115b
hsmtkk <hsmtkk@gmail.com>
2024-03-09 18:32:57 +0900

    add test API

---
+++ front/api/test/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+    console.log("output env vars")
+    console.log(process.env)
+    return NextResponse.json({ message: "ok" })
+}
